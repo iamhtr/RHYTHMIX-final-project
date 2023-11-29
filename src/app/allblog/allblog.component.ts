ca739226ef367169fe195e35fa048ba71901d4a7
minhtuuel <nguyenthanhminhtu11@gmail.com>
2023-11-29 16:59:53 +0700

    Page Product Info + All Blog

---
+++ src/app/allblog/allblog.component.ts
@@ -0,0 +1,47 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-allblog',
+  templateUrl: './allblog.component.html',
+  styleUrls: ['./allblog.component.css']
+})
+export class AllblogComponent {
+  changeBackground(id: string) {
+    const div = document.getElementById(id);
+    const button = div?.querySelector('button');
+    if (div && button) {
+      div.style.backgroundColor = '#757575';
+      button.style.backgroundColor = '#757575';
+      button.style.color = '#FFFFFF';
+    }
+  }
+
+  resetBackground(id: string) {
+    const div = document.getElementById(id);
+    const button = div?.querySelector('button');
+    if (div && button) {
+      div.style.backgroundColor = 'transparent';
+      button.style.backgroundColor = 'transparent';
+      button.style.color = '#757575';
+    }
+  }
+  changeBackground1(id: string) {
+    const div = document.getElementById(id);
+    const button = div?.querySelector('button');
+    if (div && button) {
+      div.style.backgroundColor = 'transparent';
+      button.style.backgroundColor = 'transparent';
+      button.style.color = '#757575';
+    }
+  }
+
+  resetBackground1(id: string) {
+    const div = document.getElementById(id);
+    const button = div?.querySelector('button');
+    if (div && button) {
+      div.style.backgroundColor = '#757575';
+      button.style.backgroundColor = '#757575';
+      button.style.color = '#FFFFFF';
+    }
+  }
+}
---
+++ src/app/productinfo/productinfo.component.ts
@@ -0,0 +1,61 @@
+import { Component} from '@angular/core';
+
+@Component({
+  selector: 'app-productinfo',
+  templateUrl: './productinfo.component.html',
+  styleUrl: './productinfo.component.css'
+})
+export class ProductinfoComponent {
+  quantity: number = 1;
+  initialQuantity: number = 1;
+  mainImage= "../../assets/anhalbum/tlinh-ai-album-standard_afbf68f2a8de4d70862490a725431ec6_master.webp";
+  image1 = "../../assets/anhalbum/tlinh-ai-jewel-case_b6871f8c6179429da96f24a1328ae819.webp";
+  image2 = "../../assets/anhalbum/1_7ab514a9f6a34536890cc20fe7aa4327_master.webp";
+  image3 = "../../assets/anhalbum/tlinh-ai-album-standard_afbf68f2a8de4d70862490a725431ec6_master.webp";
+  mainImageSrc: string = 'path_to_main_image.jpg';
+  magnifierVisible: boolean = false;
+  magnifierX: number = 0;
+  magnifierY: number = 0;
+
+  onIncreaseQuantity() {
+    if (this.quantity < 100) {
+      this.quantity++;
+    }
+  }
+
+  onDecreaseQuantity() {
+    if (this.quantity > 1) {
+      this.quantity--;
+    }
+  }
+
+  onQuantityChange(event: Event) {
+    const newQuantity = parseInt((event.target as HTMLInputElement).value, 10);
+
+    if (!isNaN(newQuantity) && newQuantity >= 1) {
+      this.quantity = newQuantity;
+    }
+  }
+
+  resetQuantity() {
+    this.quantity = this.initialQuantity;
+  }
+  changeMainImage(imageSrc: string) {
+    const mainImage = document.getElementById("productinfo_anhchinhablbum");
+  
+    if (mainImage instanceof HTMLElement) {
+      const imageElement = mainImage.getElementsByTagName("img")[0];
+      if (imageElement) {
+        imageElement.src = imageSrc;
+      }
+    }
+  }
+  // showMagnifier(visible: boolean) {
+    // this.magnifierVisible = visible;
+  // }
+
+  // updateMagnifier(event: MouseEvent) {
+    // this.magnifierX = event.clientX;
+    // this.magnifierY = event.clientY;
+  // }
+}
