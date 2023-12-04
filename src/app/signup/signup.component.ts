760aa76c2b21871dd3ac329c9f679f69cdbbf2fb
iamhtr <trinhdh21406c@st.uel.edu.vn>
2023-12-04 22:07:54 +0700

    Update

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,41 +1,33 @@
-import { CommonModule } from '@angular/common';
-import { Component, OnInit, inject } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { confirmPasswordValidator } from 'src/validators/confirm-password.validator';
+import { confirmPasswordValidator } from 'src/validators/check.validator';
 import { Title } from '@angular/platform-browser';
-
 
 @Component({
   selector: 'app-signup',
-
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.css']
 })
 export class SignupComponent implements OnInit {
-  //set title of page
-  public constructor(private titleService: Title){
+  // Set title of the page
+  constructor(private fb: FormBuilder, private titleService: Title) {
     this.titleService.setTitle("Sign up - Rhythmix"); 
   }
 
-  fb = inject(FormBuilder);
-
-  registerForm !: FormGroup;
+  registerForm!: FormGroup;
 
   ngOnInit(): void {
     this.registerForm = this.fb.group({
-      fullName: ['', Validators.required],
+      fullName: ['', [Validators.required, Validators.minLength(3)]],
       email: ['', Validators.compose([Validators.required, Validators.email])],
       password: ['', Validators.required],
       confirmPassword: ['', Validators.required],
-
-    },
-    {
+    }, {
       validator: confirmPasswordValidator('password', 'confirmPassword')
-    }
-    
-    )
+    });
   }
-  register(){
-    console.log(this.registerForm.value)
+
+  register() {
+    console.log(this.registerForm.value);
   }
 }
